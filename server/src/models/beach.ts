274d6e22cf5370249710088d1f75180526942127
eduardosdl <edu.silva.e08@gmail.com>
2023-03-01 11:50:56 -0300

    add user id in beach model

--- server/src/models/beach.ts
+++ server/src/models/beach.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
 
 export enum BeachPosition {
   S = 'S',
@@ -13,16 +13,16 @@ export interface BeachProps {
   position: BeachPosition;
   lat: number;
   lng: number;
+  user: string;
 }
 
-interface BeachModel extends Omit<BeachProps, '_id'>, Document {}
-
-const schema = new mongoose.Schema<BeachModel, Model<BeachModel>>(
+const schema = new Schema(
   {
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     toJSON: {
@@ -35,4 +35,5 @@ const schema = new mongoose.Schema<BeachModel, Model<BeachModel>>(
   }
 );
 
-export const Beach = mongoose.model('Beach', schema);
+interface BeachModel extends Omit<BeachProps, '_id'>, Document {}
+export const Beach = model<BeachModel>('Beach', schema);
