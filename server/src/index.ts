645a613374e7b69b792ba59770970539c2e063ab
eduardosdl <edu.silva.e08@gmail.com>
2023-03-01 11:59:23 -0300

    index to up server

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import config from 'config';
+
+import { SetupServer } from './server';
+
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'));
+  await server.init();
+  server.start();
+})();
