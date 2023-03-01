a40c0399ad5c23c0bdf57f9d5f7ffa22d4a37250
eduardosdl <edu.silva.e08@gmail.com>
2023-03-01 11:56:17 -0300

    update forecast controller to find user and send request to storm glass

--- server/src/controllers/forecast.ts
+++ server/src/controllers/forecast.ts
@@ -1,51 +1,27 @@
-import { Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
+import { Forecast } from '@src/services/forecast';
+import { Beach } from '@src/models/beach';
+import { authMiddleware } from '@src/middlewares/auth';
+
+const forecast = new Forecast();
+
 @Controller('forecast')
+@ClassMiddleware(authMiddleware)
 export class ForecastController {
   @Get('')
-  public getForecastForLoggedUser(req: Request, res: Response): void {
-    res.status(200).send([
-      {
-        time: '2023-02-12T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -8.2601,
-            lng: -34.9451,
-            name: 'Paiva',
-            position: 'E',
-            rating: 1,
-            swellDirection: 45.54,
-            swellHeight: 0.37,
-            swellPeriod: 8.94,
-            time: '2023-02-12T00:00:00+00:00',
-            waveDirection: 151.94,
-            waveHeight: 1.16,
-            windDirection: 161.9,
-            windSpeed: 1.58,
-          },
-        ],
-      },
-      {
-        time: '2023-02-12T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -8.2601,
-            lng: -34.9451,
-            name: 'Paiva',
-            position: 'E',
-            rating: 1,
-            swellDirection: 67.59,
-            swellHeight: 0.38,
-            swellPeriod: 9.77,
-            time: '2023-02-12T01:00:00+00:00',
-            waveDirection: 151.21,
-            waveHeight: 1.15,
-            windDirection: 163.25,
-            windSpeed: 1.61,
-          },
-        ],
-      },
-    ]);
+  public async getForecastForLoggedUser(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const beaches = await Beach.find({ user: req.decoded?.id });
+      const forecastData = await forecast.processForecastForBeaches(beaches);
+
+      res.status(200).send(forecastData);
+    } catch (err) {
+      res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
