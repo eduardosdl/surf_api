d2ebacac5d459ea6924309f1b014abe75512e81c
eduardosdl <edu.silva.e08@gmail.com>
2023-03-02 10:30:34 -0300

    beaches controllers

---
+++ server/src/controllers/beaches.ts
@@ -0,0 +1,26 @@
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+import { Beach } from '@src/models/beach';
+import { authMiddleware } from '@src/middlewares/auth';
+
+@Controller('beaches')
+@ClassMiddleware(authMiddleware)
+export class BeachesController {
+  @Post('')
+  public async create(req: Request, res: Response): Promise<void> {
+    try {
+      const beach = new Beach({ ...req.body, ...{ user: req.decoded?.id } });
+      const result = await beach.save();
+
+      res.status(201).send(result);
+    } catch (err) {
+      if ((err as Error) instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: (err as Error).message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
+  }
+}
