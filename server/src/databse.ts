bdb1e890895e6db1919e2a94d728361a0d471450
eduardosdl <edu.silva.e08@gmail.com>
2023-03-02 10:27:33 -0300

    add strict query

--- server/src/databse.ts
+++ server/src/databse.ts
@@ -1,10 +1,11 @@
 import config, { IConfig } from 'config';
-import { connect as mongooseConnect, connection } from 'mongoose';
+import mongoose from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
-  await mongooseConnect(dbConfig.get('mongoUrl'));
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(dbConfig.get('mongoUrl'));
 };
 
-export const close = (): Promise<void> => connection.close();
+export const close = (): Promise<void> => mongoose.connection.close();
