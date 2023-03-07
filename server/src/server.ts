1d2151158510a7af2ea5e1d8f18fd05ba947c745
eduardosdl <edu.silva.e08@gmail.com>
2023-03-07 15:55:18 -0300

    use all controllers and database, send log with logger and config shitdown server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
 import './utils/module_alias';
 import { Server } from '@overnightjs/core';
 import express, { Application } from 'express';
+import expressPino from 'express-pino-logger';
+import cors from 'cors';
+
 import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
+import { UserController } from './controllers/users';
+import * as database from '@src/databse';
+import logger from './logger';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -10,16 +17,48 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(express.json());
+    this.app.use(
+      expressPino({
+        logger,
+      })
+    );
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
   }
 
   private setupController(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    const userController = new UserController();
+
+    this.addControllers([
+      forecastController,
+      beachesController,
+      userController,
+    ]);
   }
 
-  public init(): void {
+  private async databseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupController();
+    await this.databseSetup();
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      logger.info(`Server listening of port: ${this.port}`);
+    });
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
