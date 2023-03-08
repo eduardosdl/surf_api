7a724e1af2befbf9dfc63160cc7a55c747e97335
eduardosdl <edu.silva.e08@gmail.com>
2023-03-08 12:47:26 -0300

    update base controller to handle errors and send error response

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,15 +2,27 @@ import { Response } from 'express';
 import mongoose from 'mongoose';
 
 import { CUSTOM_VALIDATION } from '@src/models/user';
+import logger from '@src/logger';
+import ApiError, { APIError } from '@src/utils/errors/api-error';
 
 export abstract class BaseController {
   protected sendCreateUpdateErrorResponse(res: Response, error: unknown): void {
     if (error instanceof mongoose.Error.ValidationError) {
       const clientErrors = this.handleClientErrors(error);
 
-      res.status(clientErrors.code).send(clientErrors);
+      res
+        .status(clientErrors.code)
+        .send(
+          ApiError.format({
+            code: clientErrors.code,
+            message: clientErrors.error,
+          })
+        );
     } else {
-      res.status(500).send({ error: 'Something went wrong' });
+      logger.error(error);
+      res
+        .status(500)
+        .send(ApiError.format({ code: 500, message: 'Something went wrong!' }));
     }
   }
 
@@ -26,5 +38,9 @@ export abstract class BaseController {
       return { code: 409, error: error.message };
     }
     return { code: 422, error: error.message };
+  }
+
+  protected sendErrorRespose(res: Response, apiError: APIError): Response {
+    return res.status(apiError.code).send(ApiError.format(apiError));
   }
 }
