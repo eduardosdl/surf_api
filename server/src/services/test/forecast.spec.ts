7d9a464ca0999cd7aebbe5f2c8a0055bd976ce72
eduardosdl <edu.silva.e08@gmail.com>
2023-03-18 10:08:08 -0300

    fix test response with rating

--- server/src/services/test/forecast.spec.ts
+++ server/src/services/test/forecast.spec.ts
@@ -9,6 +9,92 @@ jest.mock('@src/clients/stormGlass');
 describe('Forecast service', () => {
   const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
 
+  it('should return the forecast for multiple beaches in the same hour with different ratings ordered by rating', async () => {
+    mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
+      {
+        swellDirection: 123.41,
+        swellHeight: 0.21,
+        swellPeriod: 3.67,
+        time: '2023-02-12T00:00:00+00:00',
+        waveDirection: 232.12,
+        waveHeight: 0.46,
+        windDirection: 310.48,
+        windSpeed: 100,
+      },
+    ]);
+    mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
+      {
+        swellDirection: 64.26,
+        swellHeight: 0.15,
+        swellPeriod: 13.89,
+        time: '2023-02-12T00:00:00+00:00',
+        waveDirection: 231.38,
+        waveHeight: 2.07,
+        windDirection: 299.45,
+        windSpeed: 100,
+      },
+    ]);
+    const beaches: BeachProps[] = [
+      {
+        lat: -8.2601,
+        lng: -34.9451,
+        name: 'Paiva',
+        position: GeoPosition.E,
+        user: 'fake-id',
+      },
+      {
+        lat: -8.504838,
+        lng: -35.0063,
+        name: 'Porto de Galinhas',
+        position: GeoPosition.E,
+        user: 'fake-id',
+      },
+    ];
+
+    const expectedResponse = [
+      {
+        time: '2023-02-12T00:00:00+00:00',
+        forecast: [
+          {
+            lat: -8.504838,
+            lng: -35.0063,
+            name: 'Porto de Galinhas',
+            position: 'E',
+            rating: 3,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 13.89,
+            time: '2023-02-12T00:00:00+00:00',
+            waveDirection: 231.38,
+            waveHeight: 2.07,
+            windDirection: 299.45,
+            windSpeed: 100,
+          },
+          {
+            lat: -8.2601,
+            lng: -34.9451,
+            name: 'Paiva',
+            position: 'E',
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2023-02-12T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100,
+          },
+        ],
+      },
+    ];
+
+    const forecast = new Forecast(mockedStormGlassService);
+    const beachesWithRating = await forecast.processForecastForBeaches(beaches);
+
+    expect(beachesWithRating).toEqual(expectedResponse);
+  });
+
   it('should return the forecast for a list of beaches', async () => {
     mockedStormGlassService.fetchPoints.mockResolvedValue(
       stormGlassNormalizedResponseFixture
@@ -32,7 +118,7 @@ describe('Forecast service', () => {
             lng: -34.9451,
             name: 'Paiva',
             position: 'E',
-            rating: 1,
+            rating: 2,
             swellDirection: 45.54,
             swellHeight: 0.37,
             swellPeriod: 8.94,
@@ -52,7 +138,7 @@ describe('Forecast service', () => {
             lng: -34.9451,
             name: 'Paiva',
             position: 'E',
-            rating: 1,
+            rating: 2,
             swellDirection: 67.59,
             swellHeight: 0.38,
             swellPeriod: 9.77,
@@ -72,7 +158,7 @@ describe('Forecast service', () => {
             lng: -34.9451,
             name: 'Paiva',
             position: 'E',
-            rating: 1,
+            rating: 3,
             swellDirection: 89.63,
             swellHeight: 0.4,
             swellPeriod: 10.59,
